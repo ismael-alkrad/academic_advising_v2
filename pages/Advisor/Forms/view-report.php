5c97e3888200c50e0876e71021d18b1aa35b6fb4
EsmaelEmad <Esmaelalkrad0@gmail.com>
2023-05-26 21:45:57 +0300

    Edit

--- pages/Advisor/Forms/view-report.php
+++ pages/Advisor/Forms/view-report.php
@@ -45,24 +45,118 @@ check(text: "Location: ../../../index.php");
                     </h2>
                 </div>
                 <div class="col mt-3">
-                    <table id="courses-add-table" class="table table-striped text-center" dir="rtl">
-                        <thead>
-                            <tr>
-                                <th scope="col"> التقرير </th>
-                                <th scope="col"> عرض التقرير </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td class="pt-3"> تقرير الطلبة الموهوبين </td>
-                                <td> <button type="button" class="btn btn-primary">عرض</button> </td>
-                            </tr>
-                            <tr>
-                                <td class="pt-3"> تقرير اسباب التعثر الدراسي </td>
-                                <td> <button type="button" class="btn btn-primary">عرض</button> </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <ul class="nav nav-tabs" dir="rtl" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <input type="radio" class="btn-check" id="gifted-students" name="report" autocomplete="off" data-bs-toggle="tab" data-bs-target="#gifted-students-pane" role="tab" aria-controls="gifted-students-pane" aria-selected="true">
+                            <label class="nav-link btn btn-outline-primary border-primary nav-button-primary me-1 rounded-0 rounded-top" for="gifted-students"> تقرير المواهب </label>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <input type="radio" class="btn-check" id="academic-failure" name="report" autocomplete="off" data-bs-toggle="tab" data-bs-target="#academic-failure-pane" role="tab" aria-controls="academic-failure-pane" aria-selected="false">
+                            <label class="nav-link btn btn-outline-primary border-primary nav-button-primary me-1 rounded-0 rounded-top" for="academic-failure"> تقرير التعثر الدراسي </label>
+                        </li>
+                    </ul>
+                    <div class="tab-content pt-3" dir="rtl" id="myTabContent">
+                        <div class="tab-pane fade" id="gifted-students-pane" role="tabpanel" aria-labelledby="gifted-students" tabindex="0">
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">الاسم الرباعي</th>
+                                        <td><?php echo getFnameByUid(
+                                                $conn,
+                                                $_GET['student']
+                                            ); ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">رقم الهاتف </th>
+                                        <td>0791570296</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">البريد الالكتروني</th>
+                                        <td><?php echo $_GET['email'] ?></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div class="mt-5">
+                                <h4> المواهب التي يتمتع بها الطالب </h4>
+                                <div>
+                                    <table class="table table-striped text-center">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">الموهبة</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">1</th>
+                                                <td>السباحة</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">2</th>
+                                                <td>ركوب الخيل</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">3</th>
+                                                <td> كرةالقدم </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-content pt-3" dir="rtl" id="myTabContent">
+                        <div class="tab-pane fade" id="academic-failure-pane" role="tabpanel" aria-labelledby="academic-failure" tabindex="0">
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">الاسم</th>
+                                        <td><?php echo $data['name']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">الرقم الجامعي</th>
+                                        <td><?php echo $data['u_id']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row"> الفصل الدراسي / العام الجامعي للطالب </th>
+                                        <td>الأول</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row"> المعدل التراكمي </th>
+                                        <td>90.8</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <table class="table table-bordered position">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">العبارات</th>
+                                        <th scope="col" class="text-center">موافق</th>
+                                        <th scope="col" class="text-center">غير موافق</th>
+                                        <th scope="col" class="text-center">لا أعرف</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">صعوبة المواد الدراسية</th>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div>
+                                <div class="col mt-3" id="">
+                                    <label class="form-label text-start">
+                                        الحلول المقترحة
+                                    </label>
+                                    <div class="mb-3">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
